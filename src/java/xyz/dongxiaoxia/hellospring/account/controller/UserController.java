baeb47fd0d9ed489539c7b59d9f6f1a3e0c9e1b4
陈文东 <810196858@qq.com>
2015-10-30 16:55:47 +0800

    Add Spring configuration

--- src/java/xyz/dongxiaoxia/hellospring/account/controller/UserController.java
+++ src/java/xyz/dongxiaoxia/hellospring/account/controller/UserController.java
@@ -1,15 +1,30 @@
 package xyz.dongxiaoxia.hellospring.account.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import xyz.dongxiaoxia.hellospring.account.service.UserService;
 import xyz.dongxiaoxia.hellospring.core.entity.User;
 
 /**
  * Created by chenwendong on 2015/10/29.
  */
+
+@Controller
+@RequestMapping(value = "/api/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
     public static void main(String[] args) {
         UserService userService = new UserService();
         User user = userService.getUser("123");
         System.out.println(user.getName());
+    }
+
+    @RequestMapping(value = "/get")
+    public void get(@RequestParam(value = "id") String id) {
+        User user = userService.getUser(id);
     }
 }
--- src/java/xyz/dongxiaoxia/hellospring/account/service/UserService.java
+++ src/java/xyz/dongxiaoxia/hellospring/account/service/UserService.java
@@ -1,5 +1,7 @@
 package xyz.dongxiaoxia.hellospring.account.service;
 
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import xyz.dongxiaoxia.hellospring.core.entity.User;
 import xyz.dongxiaoxia.hellospring.core.repository.UserDao;
 import xyz.dongxiaoxia.hellospring.core.repository.impl.UserDaoImpl;
@@ -7,6 +9,8 @@ import xyz.dongxiaoxia.hellospring.core.repository.impl.UserDaoImpl;
 /**
  * Created by chenwendong on 2015/10/29.
  */
+@Component
+@Transactional
 public class UserService {
     private UserDao userDao = new UserDaoImpl();
 
